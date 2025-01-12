21c65b53e2c6a900505e113c95b856d1f77f7c8a
smriti243 <iamaries.smriti@gmail.com>
2025-01-12 12:47:17 +0530

    idk'

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './HomePage.css';
+import Header from './Header';
 
 function HomePage() {
   const [searchQuery, setSearchQuery] = useState('');
